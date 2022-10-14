e83ac6c653f5109ec55b208e5751c99d10ad0feb
NicolasTigle <79977687+NicolasTigle@users.noreply.github.com>
2022-10-14 13:49:17 -0300

    Delete SPraia directory

--- SPraia/src/main/java/br/com/zeniti/spraia/repository/DenunciaRepository.java
+++
@@ -1,9 +0,0 @@
-package br.com.zeniti.spraia.repository;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.zeniti.spraia.model.Denuncia;
-
-public interface DenunciaRepository extends JpaRepository<Denuncia, Long>{
-    
-}
--- SPraia/src/main/java/br/com/zeniti/spraia/repository/PraiaRepository.java
+++
@@ -1,8 +0,0 @@
-package br.com.zeniti.spraia.repository;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import br.com.zeniti.spraia.model.Praia;
-
-public interface PraiaRepository extends JpaRepository<Praia, Long>{
-    
-}
